c78a189a78b204e0b3972790f4be3030dd3b406e
timplay33 <tim3play3@gmail.com>
2023-04-22 00:12:51 +0200

    modified:   src/app/Home.tsx

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,7 +3,7 @@ export const Home = () => {
     <>
       <div className="h-screen p-3 main m-16 w-2/3 self-center">
         <h1 className="text-5xl font-bold text-headline ">
-          Hi, <br></br>I'm Tim!
+          Hi, <br></br>I&apos;m Tim!
         </h1>
         <p>This page is still in progress</p>
       </div>
